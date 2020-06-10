d62030ab3dc04f86b80773b0b68b5f627d2107a2
solwininfotech <info@solwininfotech.com>
2020-06-10 09:48:59 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2016 Solwin Infotech. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Megamenu',
+    __DIR__
+);
